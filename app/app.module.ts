97aa29ef538bf50566e75de4011a51806e34b1c2
Mckaid Gustafson <mckaidcgustafson@gmail.com>
2017-06-06 08:05:06 -0600

    Added proposal list component

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,21 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { DocumentsComponent } from './documents/documents.component';
+import { ProposalListComponent } from './proposal/proposal-list.component';
 
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  declarations: [
-    AppComponent,
-    HomepageComponent,
-    DocumentsComponent
-  ],
-  bootstrap: [
-    AppComponent
-  ]
+	imports: [
+		BrowserModule,
+		AppRoutingModule
+	],
+	declarations: [
+		AppComponent,
+		HomepageComponent,
+		DocumentsComponent,
+		ProposalListComponent
+	],
+	bootstrap: [
+		AppComponent
+	]
 })
 export class AppModule {}
