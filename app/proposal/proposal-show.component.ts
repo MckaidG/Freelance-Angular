e836244c0f501e8787600a1cd81f52ebec53cbb3
Mckaid Gustafson <mckaidcgustafson@gmail.com>
2017-06-07 14:29:59 -0600

    finished angular view

--- app/proposal/proposal-show.component.ts
+++ app/proposal/proposal-show.component.ts
@@ -3,23 +3,24 @@ import { ActivatedRoute, Params } from '@angular/router';
 import { Proposal } from './proposal';
 
 @Component({
-moduleId: module.id,
-selector: 'proposal-show',
-templateUrl: 'proposal-show.component.html'
-
+	moduleId: module.id,
+	selector: 'proposal-show',
+	templateUrl: 'proposal-show.component.html',
+	styleUrls: ['proposal-show.component.css']
 })
 export class ProposalShowComponent implements OnInit {
-  id: number;
-  routeId: any;
-  constructor(
-    private route: ActivatedRoute
-  ) {}
+	id: number;
+	routeId: any;
 
-  ngOnInit(): void {
-    this.routeId = this.route.params.subscribe(
-      params => {
-        this.id = +params['id'];
-      }
-    )
-  }
+	constructor(
+		private route: ActivatedRoute
+	) {}
+
+	ngOnInit(): void {
+		this.routeId = this.route.params.subscribe(
+				params => {
+					this.id = +params['id'];
+				}
+			)
+	}
 }
